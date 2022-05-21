946d289ae9c2f05ef9ed281a58f54d7d294384ca
Alphonse-03 <alphonseprakashbgp@gmail.com>
2022-05-21 16:48:07 +0530

    commit

--- wordle/settings.py
+++ wordle/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&44t^21r14oo3)+ua1=s@g6-+c@3*l@iuud14i7)wx1fh=g7m_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wordle-alphonse-vit21mca0062.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
